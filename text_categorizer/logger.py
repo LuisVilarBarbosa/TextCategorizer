3b3a846c67839f73e7f3e4cbd3fd964028502586
LuisVilarBarbosa <luisfernandobarbosa@live.com.pt>
2019-04-01 19:22:08 +0100

    Logger using UTF-8 instead of system default
    In Ubuntu, it was using UTF-8, but not in Windows.

--- text_categorizer/logger.py
+++ text_categorizer/logger.py
@@ -3,15 +3,16 @@ import logging
 filename="log.txt"
 logformat="%(asctime)s - %(levelname)s - %(module)s - %(funcName)s - %(message)s"
 dateformat="%Y-%m-%d %H:%M:%S %z %Z"
-logging.basicConfig(
-    filename=filename,
-    level=logging.DEBUG,
-    format=logformat,
-    datefmt=dateformat)
-logger = logging.getLogger(filename)
+
+logger = logging.getLogger()
 logger.setLevel(logging.DEBUG)
+formatter = logging.Formatter(fmt=logformat, datefmt=dateformat)
+
+file_handler = logging.FileHandler(filename, 'a', 'utf-8')
+file_handler.setFormatter(formatter)
+logger.addHandler(file_handler)
+
 console_handler = logging.StreamHandler()
 console_handler.setLevel(logging.INFO)
-formatter = logging.Formatter(logformat)
 console_handler.setFormatter(formatter)
 logger.addHandler(console_handler)
