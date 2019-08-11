74ad49aef01b560afbefcd7268daf4b8aec8cd42
LuisVilarBarbosa <luisfernandobarbosa@live.com.pt>
2019-08-11 20:50:19 +0100

    LinearSVC's predict_proba() improved
    If the dataset has only two classes, decision_function() returns only the values ​​for the second class.

--- text_categorizer/LinearSVC_proba.py
+++ text_categorizer/LinearSVC_proba.py
@@ -1,4 +1,8 @@
-# Fully based on http://www.erogol.com/predict-probabilities-sklearn-linearsvc/. (Accessed on July 29, 2019.) (Posted on November 14, 2014.)
+# Fully based on http://www.erogol.com/predict-probabilities-sklearn-linearsvc/
+# (posted on November 14, 2014, and accessed on July 29, 2019),
+# except the handling of exception "np.AxisError" which is based on the information given in
+# https://scikit-learn.org/stable/modules/generated/sklearn.svm.LinearSVC.html#sklearn.svm.LinearSVC.decision_function
+# (accessed on August 11, 2019).
 
 import numpy as np
 from sklearn.svm import LinearSVC
@@ -12,5 +16,8 @@ class LinearSVC_proba(LinearSVC):
         f = np.vectorize(self.__platt_func)
         raw_predictions = self.decision_function(X)
         platt_predictions = f(raw_predictions)
-        probs = platt_predictions / platt_predictions.sum(axis=1)[:, None]
+        try:
+            probs = platt_predictions / platt_predictions.sum(axis=1)[:, None]
+        except np.AxisError:
+            probs = np.asarray(list(map(lambda prob1: [1-prob1, prob1], platt_predictions)))
         return probs
