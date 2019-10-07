8ec6f3fe5ed1a96a2547dd05e082e02fcdf4e141
LuisVilarBarbosa <luisfernandobarbosa@live.com.pt>
2019-10-07 16:56:59 +0100

    Update test_prediction_server.py

--- tests/test_prediction_server.py
+++ tests/test_prediction_server.py
@@ -1,3 +1,4 @@
+from numpy import int32
 from tests.test_classifiers import clfs
 from text_categorizer import prediction_server
 from text_categorizer.FeatureExtractor import FeatureExtractor
@@ -20,33 +21,36 @@ def test_get_feature_weights():
     fe = FeatureExtractor()
     X = fe.vectorizer.fit_transform(corpus)
     prediction_server._feature_extractor = fe
-    lemmas = ['test', 'corpus']
+    lemmas = ['test', 'corpus', '1.']
+    value = 0
+    lemmas_list = [('1.', value), ('corpus', value), ('test', value)]
+    classes_dict = {1: lemmas_list, 2: lemmas_list, 3: lemmas_list}
     expected_values_1 = {
-        'RandomForestClassifier': [('test', 0.0), ('corpus', 0.0)],
-        'BernoulliNB': {1: [('test', -0.4054651081081645), ('corpus', -0.4054651081081645)], 2: [('test', -0.4054651081081645), ('corpus', -0.4054651081081645)], 3: [('test', -0.4054651081081645), ('corpus', -0.4054651081081645)]},
-        'MultinomialNB': {1: [('corpus', -1.5243987931498386), ('test', -1.5243987931498386)], 2: [('corpus', -1.5243987931498386), ('test', -1.5243987931498386)], 3: [('corpus', -1.5243987931498386), ('test', -1.5243987931498386)]},
-        'ComplementNB': {1: [('test', 1.4767261398842497), ('corpus', 1.4767261398842497)], 2: [('test', 1.4767261398842497), ('corpus', 1.4767261398842497)], 3: [('test', 1.4767261398842497), ('corpus', 1.4767261398842497)]},
+        'RandomForestClassifier': lemmas_list,
+        'BernoulliNB': classes_dict,
+        'MultinomialNB': classes_dict,
+        'ComplementNB': classes_dict,
         'KNeighborsClassifier': [],
         'MLPClassifier': [],
-        'LinearSVC_proba': {1: [('test', -0.08517207986343706), ('corpus', -0.08517207986343706)], 2: [('test', -0.08517793213189749), ('corpus', -0.08517793213189749)], 3: [('test', -0.08518054004436439), ('corpus', -0.08518054004436439)]},
-        'DecisionTreeClassifier': [('test', 0.0), ('corpus', 0.0)],
-        'ExtraTreeClassifier': [('test', 0.0), ('corpus', 0.0)],
+        'LinearSVC_proba': classes_dict,
+        'DecisionTreeClassifier': lemmas_list,
+        'ExtraTreeClassifier': lemmas_list,
         'DummyClassifier': [],
-        'SGDClassifier': {1: [('test', -8.888130494664434), ('corpus', -8.888130494664434)], 2: [('test', -8.88813049466443), ('corpus', -8.88813049466443)], 3: [('test', -8.888130494664434), ('corpus', -8.888130494664434)]},
+        'SGDClassifier': classes_dict,
         'BaggingClassifier': []
     }
-    lemmas_dict = {'corpus', 'test', '1.', '2.', '3.'}
-    classes_dict = {1: lemmas_dict, 2: lemmas_dict, 3: lemmas_dict}
+    lemmas_set = {('corpus', value), ('test', value), ('1.', value), ('2.', value), ('3.', value)}
+    classes_dict = {1: lemmas_set, 2: lemmas_set, 3: lemmas_set}
     expected_values_2 = {
-        'RandomForestClassifier': lemmas_dict,
+        'RandomForestClassifier': lemmas_set,
         'BernoulliNB': classes_dict,
         'MultinomialNB': classes_dict,
         'ComplementNB': classes_dict,
         'KNeighborsClassifier': set(),
         'MLPClassifier': set(),
         'LinearSVC_proba': classes_dict,
-        'DecisionTreeClassifier': lemmas_dict,
-        'ExtraTreeClassifier': lemmas_dict,
+        'DecisionTreeClassifier': lemmas_set,
+        'ExtraTreeClassifier': lemmas_set,
         'DummyClassifier': set(),
         'SGDClassifier': classes_dict,
         'BaggingClassifier': set()
@@ -60,37 +64,34 @@ def test_get_feature_weights():
         expected_value_1 = expected_values_1[clf_name]
         expected_value_2 = expected_values_2[clf_name]
         assert prediction_server._feature_weights.get(clf_name) is None
-        assert all(v in expected_value_1 for v in prediction_server.get_feature_weights(clf, lemmas)['feature_weights'])
-        fw = prediction_server._feature_weights[clf_name]
-        assert all(v in fw for v in expected_value_1)
-        if type(fw) is dict:
-            for k in fw.keys():
-                assert len(fw[k]) > len(expected_value_1)
-        else:
-            assert len(fw) > len(expected_value_1) or (len(fw) == 0 and len(expected_value_1) == 0)
-        assert replace_tuples_by_keys(fw) == expected_value_2
+        fw1 = prediction_server.get_feature_weights(clf, lemmas)['feature_weights']
+        assert replace_tuples_values(fw1, value=value) == expected_value_1
+        assert check_tuples_order(fw1)
+        fw2 = prediction_server._feature_weights[clf_name]
+        assert replace_tuples_values(fw2, value=value) == expected_value_2
 
 def test_load_feature_weights():
     corpus = [['Test corpus 1.', 'Test corpus 2.'], ['Test corpus 1.', 'Test corpus 2.', 'Test corpus 3.']]
     classifications = [[1, 2], [1, 2, 3]]
-    lemmas_dicts = [{'corpus', 'test', '1.', '2.'}, {'corpus', 'test', '1.', '2.', '3.'}]
-    classes_dicts = [{0: lemmas_dicts[0]}, {1: lemmas_dicts[1], 2: lemmas_dicts[1], 3: lemmas_dicts[1]}]
+    value = 0
+    lemmas_sets = [{('corpus', value), ('test', value), ('1.', value), ('2.', value)}, {('corpus', value), ('test', value), ('1.', value), ('2.', value), ('3.', value)}]
+    classes_dicts = [{0: lemmas_sets[0]}, {1: lemmas_sets[1], 2: lemmas_sets[1], 3: lemmas_sets[1]}]
     fe_hashing = FeatureExtractor(vectorizer_name='HashingVectorizer')
     fe_tfidf = FeatureExtractor(vectorizer_name='TfidfVectorizer')
     for i in range(2):
         X = fe_tfidf.vectorizer.fit_transform(corpus[i])
-        lemmas_dict = lemmas_dicts[i]
+        lemmas_set = lemmas_sets[i]
         classes_dict = classes_dicts[i]
         expected_values = {
-            'RandomForestClassifier': lemmas_dict,
+            'RandomForestClassifier': lemmas_set,
             'BernoulliNB': classes_dict,
             'MultinomialNB': classes_dict,
             'ComplementNB': classes_dict,
             'KNeighborsClassifier': set(),
             'MLPClassifier': set(),
             'LinearSVC_proba': classes_dict,
-            'DecisionTreeClassifier': lemmas_dict,
-            'ExtraTreeClassifier': lemmas_dict,
+            'DecisionTreeClassifier': lemmas_set,
+            'ExtraTreeClassifier': lemmas_set,
             'DummyClassifier': set(),
             'SGDClassifier': classes_dict,
             'BaggingClassifier': set()
@@ -104,18 +105,38 @@ def test_load_feature_weights():
             assert prediction_server.load_feature_weights(clf) == set()
             clf.fit(X, classifications[i])
             prediction_server._feature_extractor = fe_tfidf
-            assert replace_tuples_by_keys(prediction_server.load_feature_weights(clf)) == expected_values[clf_name]
+            assert replace_tuples_values(prediction_server.load_feature_weights(clf), value=value) == expected_values[clf_name]
 
 def test_main():
     pass
 
-def replace_tuples_by_keys(obj):
-    if type(obj) is dict:
+def replace_tuples_values(obj, value):
+    t = type(obj)
+    if t is tuple:
+        return (obj[0], value)
+    elif t is set:
+        l = [replace_tuples_values(v, value) for v in obj]
+        return set(l)
+    elif t is list:
+        l = [replace_tuples_values(v, value) for v in obj]
+        l.sort()
+        return l
+    elif t is dict:
         d = dict()
         for k, v in obj.items():
-            d[k] = set(map(lambda item: item[0], v))
+            d[replace_tuples_values(k, value)] = replace_tuples_values(v, value)
         return d
-    elif type(obj) is set:
-        return set(map(lambda item: item[0], obj))
+    elif t is int or t is int32:
+        return obj
     else:
-        raise ValueError(obj)
+        raise TypeError(t)
+
+def check_tuples_order(obj):
+    t = type(obj)
+    if t is list:
+        print(obj)
+        return obj == sorted(obj, key=lambda item: -item[1])
+    elif t is dict:
+        return all([check_tuples_order(elem) for elem in obj.values()])
+    else:
+        raise TypeError(t)
